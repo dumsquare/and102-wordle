3abd6233f6700c9d27256143b31dbfc65cdac568
dumsquare <36097260+dumsquare@users.noreply.github.com>
2024-03-02 02:21:17 -0500

    Initial commit

---
+++ app/src/main/java/com/example/and102_wordle/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.and102_wordle
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "and102-wordle"
+include(":app")
+ 
